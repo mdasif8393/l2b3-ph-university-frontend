14d644e7787ecc4a9cb077732a5190fec2291f98
mdasif8393 <md.asif.8393@gmail.com>
2024-07-05 11:56:37 +0600

    add cookie

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,40 @@
+import { Button } from "antd";
+import { useForm } from "react-hook-form";
+import { useLoginMutation } from "../redux/features/auth/authApi";
+
 const Login = () => {
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      userId: "A-0001",
+      password: "admin123",
+    },
+  });
+
+  const [login, { data, isError }] = useLoginMutation();
+
+  console.log("data =>", data);
+  console.log("error =>", isError);
+
+  const onSubmit = (data) => {
+    const userInfo = {
+      id: data.userId,
+      password: data.password,
+    };
+    login(userInfo);
+  };
+
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div>
+        <label htmlFor="id">ID:</label>
+        <input type="text" id="id" {...register("userId")} />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input type="text" id="password" {...register("password")} />
+      </div>
+      <Button htmlType="submit">Login</Button>
+    </form>
   );
 };
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { baseApi } from "./baseApi";
 import authReducer from "./features/auth/authSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
