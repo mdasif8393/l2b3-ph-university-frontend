88ecab1abd3db31d87c9e86132d4775136d3d4af
mdasif8393 <md.asif.8393@gmail.com>
2024-08-19 22:22:02 +0600

    32-10 Resolve sidebar item generator function error and module wrap-up

--- src/pages/admin/userManagement/StudentData.tsx
+++ src/pages/admin/userManagement/StudentData.tsx
@@ -7,21 +7,24 @@ import {
   TableProps,
 } from "antd";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useGetAllStudentsQuery } from "../../../redux/features/admin/userManagement.api";
 import { TQueryParam, TStudent } from "../../../types";
 
-export type TTableData = Pick<TStudent, "fullName" | "id">;
+export type TTableData = Pick<
+  TStudent,
+  "fullName" | "id" | "email" | "contactNo"
+>;
 
 const StudentData = () => {
   const [params, setParams] = useState<TQueryParam[]>([]);
-  const [page, setPage] = useState(2);
+  const [page, setPage] = useState(1);
 
   const {
     data: studentData,
     isLoading,
     isFetching,
   } = useGetAllStudentsQuery([
-    { name: "limit", value: 2 },
     { name: "page", value: page },
     { name: "sort", value: "id" },
     ...params,
@@ -29,11 +32,15 @@ const StudentData = () => {
 
   const metaData = studentData?.meta;
 
-  const tableData = studentData?.data?.map(({ fullName, _id, id }) => ({
-    key: _id,
-    fullName,
-    id,
-  }));
+  const tableData = studentData?.data?.map(
+    ({ fullName, _id, id, email, contactNo }) => ({
+      key: _id,
+      fullName,
+      id,
+      email,
+      contactNo,
+    })
+  );
 
   const columns: TableColumnsType<TTableData> = [
     {
@@ -46,15 +53,28 @@ const StudentData = () => {
       key: "id",
       dataIndex: "id",
     },
+    {
+      title: "Email",
+      key: "email",
+      dataIndex: "email",
+    },
+    {
+      title: "Contact No..",
+      key: "contactNo",
+      dataIndex: "contactNo",
+    },
 
     {
       title: "Action",
       key: "x",
-      render: () => {
+      render: (item) => {
         return (
           <Space>
+            <Link to={`/admin/student-data/${item.key}`}>
+              <Button>Details</Button>
+            </Link>
+
             <Button>Update</Button>
-            <Button>Details</Button>
             <Button>Block</Button>
           </Space>
         );
