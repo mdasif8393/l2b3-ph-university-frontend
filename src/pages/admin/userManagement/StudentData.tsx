c020809e7c85f05c791193edf6983a9b8d45f201
mdasif8393 <md.asif.8393@gmail.com>
2024-08-19 17:24:37 +0600

    32-6 Show Student data by refactoring the old table

---
+++ src/pages/admin/userManagement/StudentData.tsx
@@ -0,0 +1,86 @@
+import { Button, Space, Table, TableColumnsType, TableProps } from "antd";
+import { useState } from "react";
+import { useGetAllStudentsQuery } from "../../../redux/features/admin/userManagement.api";
+import { TQueryParam, TStudent } from "../../../types";
+
+export type TTableData = Pick<TStudent, "name" | "id">;
+
+const StudentData = () => {
+  const [params, setParams] = useState<TQueryParam[] | undefined>(undefined);
+
+  const {
+    data: studentData,
+    isLoading,
+    isFetching,
+  } = useGetAllStudentsQuery(params);
+
+  const tableData = studentData?.data?.map(({ fullName, _id, id }) => ({
+    key: _id,
+    fullName,
+    id,
+  }));
+
+  const columns: TableColumnsType<TTableData> = [
+    {
+      title: "Name",
+      key: "name",
+      dataIndex: "fullName",
+    },
+    {
+      title: "Roll No.",
+      key: "id",
+      dataIndex: "id",
+    },
+
+    {
+      title: "Action",
+      key: "x",
+      render: () => {
+        return (
+          <Space>
+            <Button>Update</Button>
+            <Button>Details</Button>
+            <Button>Block</Button>
+          </Space>
+        );
+      },
+      width: "1%",
+    },
+  ];
+
+  const onChange: TableProps<TTableData>["onChange"] = (
+    _pagination,
+    filters,
+    _sorter,
+    extra
+  ) => {
+    if (extra.action === "filter") {
+      const queryParams: TQueryParam[] = [];
+
+      filters.name?.forEach((item) =>
+        queryParams.push({ name: "name", value: item })
+      );
+      filters.year?.forEach((item) =>
+        queryParams.push({ name: "year", value: item })
+      );
+
+      setParams(queryParams);
+    }
+  };
+
+  if (isLoading) {
+    <p>Loading...</p>;
+  }
+
+  return (
+    <Table
+      loading={isFetching}
+      columns={columns}
+      dataSource={tableData}
+      onChange={onChange}
+      showSorterTooltip={{ target: "sorter-icon" }}
+    />
+  );
+};
+
+export default StudentData;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,2 +1,4 @@
+export * from "./academicManagement.type";
 export * from "./global";
 export * from "./sidebar.type";
+export * from "./userManagement.type";
